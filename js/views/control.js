65aeafe672b24fb8f625f77a87140e34acf51101
Mike Ball <mikedball@gmail.com>
2013-05-01 01:01:03 -0400

    location.hash now set on form submission

--- js/views/control.js
+++ js/views/control.js
@@ -3,20 +3,47 @@ if (typeof app === 'undefined' || !app) {
 }
 
 app.ControlView = Backbone.View.extend({
+  el: $('#controls'),
+
+  events: {
+    'submit form#control-form': 'submit'
+  },
+
   initialize: function (opts) {
     this.template = _.template($('#control-template').html());
     this.traits = opts.traits || [];
   },
 
-  el: $('#controls form'),
-
   render: function () {
     if (_.contains(this.traits, this.model.get('trait'))) {
-      this.$el.append(this.template({
+      this.$el.find('form div').append(this.template({
         trait: this.model.get('trait'),
         values: this.model.get('values')
       }));
       return this;
     }
+  },
+
+  getParams: function ($array) {
+    var i,
+        arrLength = $array.length
+        values = [];
+
+    for (i=0; i<=arrLength; i++) {
+      values.push($($array[i]).attr('value'));
+    }
+
+    return values;
+  },
+
+  buildURL: function (params) {
+    return params.join('/')
+  },
+
+  submit: function (e) {
+    e.preventDefault();
+    var params = this.getParams(this.$('select :selected'));
+
+    location.hash = 'filtered/' + this.buildURL(params);
   }
 });
