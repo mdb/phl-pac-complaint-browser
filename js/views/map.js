c1402f2db0563eccbdba3e5600df7777b7fd17a4
Mike Ball <mikedball@gmail.com>
2013-05-01 21:13:44 -0400

    Cleaned up map; now using PointsView

--- js/views/map.js
+++ js/views/map.js
@@ -3,66 +3,44 @@ if (typeof app === 'undefined' || !app) {
 }
 
 app.MapView = Backbone.View.extend({
+  el: $('#app-container'),
+
   initialize: function (opts) {
     this.config = opts;
-    this.mapOptions = this.mergeMapOptions(opts);
   },
 
-  render: function (callback) {
+  render: function () {
     var self = this;
 
-    wax.tilejson(self.config.tileURL, function(tileJSON) {
-      self.map = new L.Map(self.config.mapContainer, self.config.mapOptions)
-        .addLayer(new wax.leaf.connector(tileJSON))
-        .setView(self.config.mapOptions.center, self.config.mapOptions.zoom);
-      
-      self.map.attributionControl.addAttribution('Map Data: (c) <a href="http://www.openstreetmap.org">OpenStreetMap</a>');
-      self.setEvents();
-      if(callback && typeof callback === 'function') { callback(); }
+    wax.tilejson(self.config.tileURL, function(tile) {
+      self.drawMap(tile);
+      app.pointsView = new app.PointsView({
+        mapView: self,
+        collection: app.complaintsCollection
+      });
+      app.pointsView.render();
+      self.renderControls();
     });
   },
 
-  // Merge url params with settings set by user
-  // URL params take preference
-  mergeMapOptions: function(opts) {
-    var self = this,
-        mapParams = {},
-        urlParams = decodeURIComponent(location.hash.substring(1)).trim().split('/');
-
-    if(urlParams[1] && typeof Number(urlParams[1]) === "number") {
-      mapParams = {
-        zoom: urlParams[1],
-        center: [urlParams[2], urlParams[3]]
-      };
-    }
-
-    _.defaults(mapParams, self.mapOptions);
-
-    return mapParams;
-  },
-  
-  // Listen for changes as user pans and zoom on the map
-  setEvents: function() {
+  renderControls: function () {
     var self = this;
 
-    self.map
-      .on('zoomend', function(e) {
-        self.updateURL();
-      })
-      .on('dragend', function(e) {
-        self.updateURL();
-      });
+    self.config.controls.each(function (control) {
+      var controlView = new app.ControlView({
+        model: control,
+        controlTraits: self.config.controlTraits
+      }).render();
+    });
   },
 
-  // Gets the current map center and zoom and sets
-  // those values in the url
-  // i.e. #zoom=12&lat=39.976&lng=-75.172
-  updateURL: function() {
-    var zoom = this.map.getZoom(),
-        lat = this.map.getCenter().lat.toFixed(3),
-        lng = this.map.getCenter().lng.toFixed(3),
-        params = '/' + zoom + '/' + lat + '/' + lng;
+  drawMap: function (tile) {
+    var self = this;
 
-    location.hash = params;
+    self.map = new L.Map(self.config.mapContainer, self.config.mapOptions)
+      .addLayer(new wax.leaf.connector(tile))
+      .setView(self.config.mapOptions.center, self.config.mapOptions.zoom);
+
+    self.map.attributionControl.addAttribution('Map Data: (c) <a href="http://www.openstreetmap.org">OpenStreetMap</a>');
   }
 });
