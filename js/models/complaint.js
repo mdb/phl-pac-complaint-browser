15d8b9922f428ee133da26c69ab83e4a512db3d7
Mike Ball <mikedball@gmail.com>
2013-05-04 13:39:11 -0400

    Cleaned up complaint model; no longer using helper methods

--- js/models/complaint.js
+++ js/models/complaint.js
@@ -2,36 +2,4 @@ if (typeof app === 'undefined' || !app) {
   var app = {};
 }
 
-app.Complaint = Backbone.Model.extend({
-  getStatus: function () {
-    return this.get('status');
-  },
-
-  getRace: function () {
-    return this.get('race');
-  },
-
-  getSex: function () {
-    return this.get('sex');
-  },
-
-  getType: function () {
-    return this.get('type');
-  },
-
-  getAge: function () {
-    return this.get('age');
-  },
-
-  getUnit: function () {
-    return this.get('unit');
-  },
-
-  getDate: function () {
-    return this.get('date');
-  },
-
-  getAction: function () {
-    return this.get('action');
-  },
-});
+app.Complaint = Backbone.Model.extend({});
