6e35bf6ce882fad77494e3740e18524d4a0120ce
Mike Ball <mikedball@gmail.com>
2014-12-04 08:18:55 -0500

    build process renames demo.html > index.html

--- tasks/build.js
+++ tasks/build.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp'),
+    rename = require('gulp-rename'),
     vulcanize = require('gulp-vulcanize');
 
 module.exports = function (gulp) {
   var buildDir = 'build';
 
   gulp.task(buildDir, function () {
-    return gulp.src('src/index.html')
+    return gulp.src('src/demo.html')
+      .pipe(rename('index.html'))
       .pipe(vulcanize({
         dest: buildDir,
         strip: true,
